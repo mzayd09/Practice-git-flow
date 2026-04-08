260b4e76c5bf84e7c10e603d0d3daaee005a5547
mzayd09 <mohd.zayd@newvision-software.com>
2026-04-08 11:42:27 +0530

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-//editing file before taking pull//
+//editing file before taking pull 2//
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
