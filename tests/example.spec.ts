ffd8735196f2cb6512f983e2f7bb82acfdf2242b
mzayd09 <mohd.zayd@newvision-software.com>
2026-04-08 15:24:31 +0530

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,7 +8,7 @@ test('has title', async ({ page }) => {
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.asasas//
+  // test//
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
